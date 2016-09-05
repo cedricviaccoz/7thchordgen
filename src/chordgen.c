96d5d3439b831f16eb01c73a28da255b8884276c
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-09-06 00:53:32 +0200

    end of project

--- src/chordgen.c
+++ src/chordgen.c
@@ -1,9 +1,73 @@
 #include "chordgen.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 int main()
 {
-    printf("%s", c_forms_sixth[0]);
+  /*so the rng function has a distinctive seed each time
+  the program is run.*/
+  rng_init();
+
+  /*we will juste change the variable of this statically
+  alocated chord for the benefits of the program.*/
+  Chord chord;
+
+  /*I don't see any benefit in adding a termination feature for
+   the moment,so the program will forever keep on running.*/
+  while(1){
+    press_enter_to_continue();
+    chord.note = rng(MAX_NOTES);
+    chord.corde = rng(MAX_CORDES);
+    chord.accord = rng(MAX_CHORDS);
+    printf("\nYou need to play this chord : %s%s on the %s string.\n\n",
+            array_note[chord.note], array_accord[chord.accord],
+            array_corde[chord.corde]);
+    press_enter_to_continue();
+
+    /*now we extract the good chord diagram, make a copy and remplace
+    the "x" with the good corresponding number on thie copy*/
+    const char * correct_diagram = get_the_good_string(chord.corde,chord.accord);
+    char chord_diagram[CRD_STR_LGTH];
+    strncpy(chord_diagram, correct_diagram, CRD_STR_LGTH);
+    char xSubstitute = get_the_good_frette(chord.note, chord.corde);
+    chord_diagram[TRUTH_POS] = xSubstitute;
+    printf("\nthe solution was :\n\n%s\n", chord_diagram);
+  }
     return 0;
+}
+
+const char * get_the_good_string(Corde corde, Accord accord){
+  switch(corde){
+    case FOURTH: return c_forms_fourth[accord];
+    case FIFTH: return c_forms_fifth[accord];
+    case SIXTH: return c_forms_sixth[accord];
+    default: return c_forms_sixth[accord];
+  }
+}
+
+char get_the_good_frette(Note note, Corde corde){
+  switch(corde){
+    //+2 and +7 for the semitones in change between strings.
+    case FOURTH: return array_frette[((note + 2) % 12)];
+    case FIFTH: return array_frette[((note + 7) % 12)];
+    case SIXTH: return array_frette[note];
+    default: return array_frette[note];
+  }
+}
+
+int rng(int const limit){
+    return rand() % limit;
+}
+
+void rng_init(){
+      time_t t;
+      /* Intializes random number generator */
+      srand((unsigned) time(&t));
+}
+
+void press_enter_to_continue(){
+  printf("press ENTER key to continue.");
+  getchar();
 }
--- src/chordgen.h
+++ src/chordgen.h
@@ -1,18 +1,38 @@
 #define MAX_NOTES 12
 #define MAX_CORDES 3  //à changer en 4 plus tard.
 #define MAX_CHORDS 5
-#include <stdlib.h>
-#include <stdio.h>
+#define CRD_STR_LGTH 42
+#define TRUTH_POS 16 // where is "x" located in chords diagram.
+
+/* there are the ascii strings representation of the chords diagram,
+  the "x" at the middle case is supposed to be remplaced by the number
+  of the string when needed"*/
 
 const char * c_forms_sixth[MAX_CHORDS] = {
   " ||||||\n ||||||\nx*|||*|\n ||**||\n ||||||\n",
-  " ||||||\n ||||||\nx*|||*|\n ||**||\n ||||||\n",
-  " ||||||\n ||||||\nx*|||*|\n ||**||\n ||||||\n",
-  " ||||||\n ||||||\nx*|||*|\n ||**||\n ||||||\n",
-  " ||||||\n ||||||\nx*|||*|\n ||**||\n ||||||\n"
+  " ||||||\n ||||||\nx*|*|*|\n |||*||\n ||||||\n",
+  " ||||||\n ||||||\nx*|***|\n ||||||\n ||||||\n",
+  " ||||||\n ||||*|\nx*|**||\n ||||||\n ||||||\n",
+  " ||||||\n ||*-*|\nx*|*|||\n ||||||\n ||||||\n"
 };
 
+const char * c_forms_fifth[MAX_CHORDS] = {
+  " ||||||\n ||||||\nx|*---*\n |||*||\n ||*|*|\n",
+  " ||||||\n ||*|||\nx|*|*||\n ||||||\n ||||*|\n",
+  " ||*|||\n ||||||\nx|*|*||\n ||||*|\n ||||||\n",
+  " ||||||\n ||||||\nx|*|*||\n ||*|*|\n ||||||\n",
+  " ||||||\n |||*||\nx|*||||\n ||*|*|\n ||||||\n"
+};
+
+const char * c_forms_fourth[MAX_CHORDS] = {
+  " ||||||\n ||||||\nx||*|||\n ||||||\n |||*-*\n",
+  " ||||||\n ||||||\nx||*|||\n ||||*|\n |||*|*\n",
+  " ||||||\n ||||||\nx||*|||\n ||||**\n |||*||\n",
+  " ||||||\n ||||||\nx||*|||\n |||*-*\n ||||||\n",
+  " ||||||\n ||||||\nx||*|*|\n |||*|*\n ||||||\n",
+};
 /* enum pour représenter la tonique de l'accord.
+  followed by its array of string
    starts on F for reason, shut up.*/
 typedef enum Note Note;
 enum Note{
@@ -28,8 +48,17 @@ enum Note{
     Eb,
     E
 };
+char * array_note[MAX_NOTES] = {
+  "F", "F#", "G", "Ab", "A", "Bb", "B", "C", "C#","D", "Eb", "E"
+};
 
-//enum pour représenter sur quelle corde se trouve la tonique
+//to replace the "x" in the ASCII diagram
+char array_frette[MAX_NOTES] = {
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C'
+};
+
+/*enum pour représenter sur quelle corde se trouve la tonique.
+suivi par son tableau de string*/
 typedef enum Corde Corde;
 enum Corde{
     //THIRD,  à ajouter plus tard quand mon prof l'aura dessiné
@@ -37,8 +66,13 @@ enum Corde{
     FIFTH,
     SIXTH
 };
+char * array_corde[MAX_CORDES] = {
+  "fourth",
+  "fifth",
+  "sixth"
+};
 
-//enum pour représenter l'accord
+//enum pour représenter l'accord suivi par son tableau de string.
 typedef enum Accord Accord;
 enum Accord{
     MAJ7,
@@ -46,6 +80,13 @@ enum Accord{
     MIN7,
     m7b5,
     diminished
+};
+char * array_accord[MAX_CHORDS] = {
+  "Maj7",
+  "7",
+  "m7",
+  "m7b5",
+  "°"
 };
 
 /*Struct that represents the chord to be displayed*/
@@ -61,6 +102,17 @@ struct Chord{
    using the rng function.*/
 void rng_init();
 
+/*dumb function to make the user control at which moment
+he wants the program to procede to the next step*/
+void press_enter_to_continue();
 
 //retourne un entier choisi aléatoirement compris entre 0 et la limite donné en argument
 int rng(int const limit);
+
+/*return a pointer on the correct chord diagram according
+to the string of the base of the chord and the chord mode*/
+const char * get_the_good_string(Corde corde, Accord accord);
+
+/*return the correct frette where the base of the chord should be,
+according on the string where the first note of the chord is played.*/
+char get_the_good_frette(Note note, Corde corde);
