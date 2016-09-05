437a41f2cb8af9e31196376674feec3fb3a4a0aa
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-09-05 22:55:49 +0200

    progression et corrections

--- src/chordgen.c
+++ src/chordgen.c
@@ -1,16 +1,9 @@
+#include "chordgen.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
-    printf("Hello, World!\n");
-
+    printf("%s", c_forms_sixth[0]);
     return 0;
-}
-
-int rng(int const limit){
-    time_t t;
-    /* Intializes random number generator */
-    srand((unsigned) time(&t));
-    return rand() % limit;
-
 }
