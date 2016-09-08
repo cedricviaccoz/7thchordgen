962594886baf190d03ac154a0927090ef7ad7fe5
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-09-08 16:35:11 +0200

    added third string and driagrams

--- src/chordgen.h
+++ src/chordgen.h
@@ -1,5 +1,5 @@
 #define MAX_NOTES 12 //tweleve diatonics notes.
-#define MAX_CORDES 3  //change to 4 later.
+#define MAX_CORDES 4 // E A D G, the fourth lower strings
 #define MAX_CHORDS 5  //five mode of seventh's chord.
 #define CRD_STR_LGTH 42 //max chord diagram's string length
 #define TRUTH_POS 16 // where is "x" located in chords diagram.
@@ -7,31 +7,40 @@
 #define SOLUT_STR "see the solution"
 
 /* these are the ascii strings representation of the chords diagram,
+  "+" represents the place where the tonique is hold, and
   the "x" at the middle case is supposed to be remplaced by the number
   of the fret when needed"*/
 
 const char * c_forms_sixth[MAX_CHORDS] = {
-  " ||||||\n ||||||\nx*|||*|\n ||**||\n ||||||\n",
-  " ||||||\n ||||||\nx*|*|*|\n |||*||\n ||||||\n",
-  " ||||||\n ||||||\nx*|***|\n ||||||\n ||||||\n",
-  " ||||||\n ||||*|\nx*|**||\n ||||||\n ||||||\n",
-  " ||||||\n ||*-*|\nx*||*||\n ||||||\n ||||||\n"
+  " ||||||\n ||||||\nx+|||*|\n ||**||\n ||||||\n",
+  " ||||||\n ||||||\nx+|*|*|\n |||*||\n ||||||\n",
+  " ||||||\n ||||||\nx+|***|\n ||||||\n ||||||\n",
+  " ||||||\n ||||*|\nx+|**||\n ||||||\n ||||||\n",
+  " ||||||\n ||*-*|\nx+||*||\n ||||||\n ||||||\n"
 };
 
 const char * c_forms_fifth[MAX_CHORDS] = {
-  " ||||||\n ||||||\nx|*---*\n |||*||\n ||*|*|\n",
-  " ||||||\n ||*|||\nx|*|*||\n ||||||\n ||||*|\n",
-  " ||*|||\n ||||||\nx|*|*||\n ||||*|\n ||||||\n",
-  " ||||||\n ||||||\nx|*|*||\n ||*|*|\n ||||||\n",
-  " ||||||\n |||*||\nx|*||||\n ||*|*|\n ||||||\n"
+  " ||||||\n ||||||\nx|+---*\n |||*||\n ||*|*|\n",
+  " ||||||\n ||*|||\nx|+|*||\n ||||||\n ||||*|\n",
+  " ||*|||\n ||||||\nx|+|*||\n ||||*|\n ||||||\n",
+  " ||||||\n ||||||\nx|+|*||\n ||*|*|\n ||||||\n",
+  " ||||||\n |||*||\nx|+||||\n ||*|*|\n ||||||\n"
 };
 
 const char * c_forms_fourth[MAX_CHORDS] = {
-  " ||||||\n ||||||\nx||*|||\n ||||||\n |||*-*\n",
-  " ||||||\n ||||||\nx||*|||\n ||||*|\n |||*|*\n",
-  " ||||||\n ||||||\nx||*|||\n ||||**\n |||*||\n",
-  " ||||||\n ||||||\nx||*|||\n |||*-*\n ||||||\n",
-  " ||||||\n ||||||\nx||*|*|\n |||*|*\n ||||||\n",
+  " ||||||\n ||||||\nx||+|||\n ||||||\n |||*-*\n",
+  " ||||||\n ||||||\nx||+|||\n ||||*|\n |||*|*\n",
+  " ||||||\n ||||||\nx||+|||\n ||||**\n |||*||\n",
+  " ||||||\n ||||||\nx||+|||\n |||*-*\n ||||||\n",
+  " ||||||\n ||||||\nx||+|*|\n |||*|*\n ||||||\n"
+};
+
+const char * c_forms_third[MAX_CHORDS] = {
+  " ||||||\n ||||||\nx||*+-*\n ||||||\n |||||*\n",
+  " ||||||\n ||||||\nx||*+-*\n |||||*\n ||||||\n",
+  " ||||||\n ||||*|\nx||*+||\n ||||||\n |||||*\n",
+  " ||||||\n ||*-*|\nx|||+||\n |||||*\n ||||||\n",
+  " ||||||\n ||*|*|\nx|||+|*\n ||||||\n ||||||\n"
 };
 /* enum which represents the base of the chord.
   followed by its array of string
@@ -56,7 +65,7 @@ char * array_note[MAX_NOTES] = {
 };
 
 /*to replace the "x" in the ASCII diagram
-  Since I can't write "10", "11" and "12" as one char, 
+  Since I can't write "10", "11" and "12" as one char,
   I use Hexadeciaml notation to represent them*/
 char array_frette[MAX_NOTES] = {
   '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C'
@@ -65,13 +74,14 @@ char array_frette[MAX_NOTES] = {
 /*enum pour représenter sur quelle corde se trouve la tonique.
 suivi par son tableau de string*/
 typedef enum Corde{
-    //THIRD,  à ajouter plus tard quand mon prof l'aura dessiné
+    THIRD,
     FOURTH,
     FIFTH,
     SIXTH
 } Corde;
 
 char * array_corde[MAX_CORDES] = {
+  "third",
   "fourth",
   "fifth",
   "sixth"
@@ -82,8 +92,8 @@ typedef enum Accord{
     MAJ7,
     SEVEN,
     MIN7,
-    m7b5,
-    diminished
+    M7B5,
+    DIMINISHED
 } Accord;
 
 char * array_accord[MAX_CHORDS] = {
@@ -104,10 +114,10 @@ typedef struct Chord{
 /* initialize the rng with system's time as seed.
    Must imperatively be called once in the main before
    using the rng function.*/
-void rng_init();
+void rng_init(void);
 
 /*dumb function to make the user control at which moment
-he wants the program to procede to the next step, 
+he wants the program to procede to the next step,
 takes a string as argument to modularize what comes
 after "press enter key to " */
 void press_enter_to_continue(const char * strng);
@@ -122,3 +132,8 @@ const char * get_the_good_string(Corde corde, Accord accord);
 /*return the correct frette where the base of the chord should be,
 according on the string where the first note of the chord is played.*/
 char get_the_good_fret(Note note, Corde corde);
+
+/*prints the error message in case one of the two above
+  functions would enter the default case. It should never
+  happen in any case but who knows, shit happens.*/
+void errorMsg(void);
